0fdb77f16cda19ae524b242bbb9c34d1a9c85553
Roman Lysenko <romko.lisenko@gmail.com>
2019-02-15 13:19:45 +0200

    Deleted server.js and moved all its functionality to app.js; Deleted all useless files

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const categoryrouter = require('./category/categoryrouter');
 const articleRouter = require('./articte/router');
+app.listen(3000, () => { console.log("Server is running") });
 mongoose.connect(
     'mongodb://localhost/tast1',
     {
@@ -13,9 +14,10 @@ mongoose.connect(
         console.log('Connect to MongoDb...');
     }
 );
+app.use('/',(req,res)=>{res.status(200).json({"message":"You are at my Blog"})})
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/categories', categoryrouter);
-app.use('/article',articleRouter)
+app.use('/article', articleRouter)
 module.exports = app; 
